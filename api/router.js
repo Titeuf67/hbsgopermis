e7195a1c2071579b1bddc46e081b72f35515156d
Titeuf67 <christian.fillette@gmail.com>
2022-07-19 16:43:23 +0200

    19/07

--- api/router.js
+++ api/router.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 // Router
 router.get('/', function (req, res) {
+    // res.redirect('/')
     res.render('home', {
         users: fkdb.users
     })
@@ -12,7 +13,12 @@ router.get('/', function (req, res) {
 router.get('/inscription', function (req, res) {
     res.render('inscription')
 })
-
+router.get('/pageIdAuto', function (req, res) {
+    res.render('pageIdAuto')
+})
+router.get('/premiereinscription', function (req, res) {
+    res.render('premiereinscription')
+})
 router.get('/admin', function (req, res) {
     res.render("admin", {
         // Quand nous utilisons un layout qui n'est pas celui par default nous devons le spécifié
